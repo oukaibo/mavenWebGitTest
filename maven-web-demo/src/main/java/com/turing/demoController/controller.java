30462b72b204334f38856c83f381f5e29715a8f2
苏亚飞 <syf@163.com>
2019-04-02 16:46:39 +0800

    first commit_okb

---
+++ maven-web-demo/src/main/java/com/turing/demoController/controller.java
@@ -0,0 +1,5 @@
+package com.turing.demoController;
+
+public class controller {
+
+}
---
+++ maven-web-demo/src/test/java/test_okb/Demo1.java
@@ -0,0 +1,12 @@
+package test_okb;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<5;i++) {
+			System.out.println("aaaaaa");
+		}
+	}
+
+}
