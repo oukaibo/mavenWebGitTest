e81e8815b1b3d679e3a957997fbd33366cbceb04
bokaiou <okb2017@163.com>
2019-04-04 15:41:38 +0800

    [ADD]Demo4直接commit+push

---
+++ maven-web-demo/src/test/java/test_okb/Demo4.java
@@ -0,0 +1,13 @@
+package test_okb;
+
+public class Demo4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<5;i++) {
+			//测试拉取项目，操作步骤
+			System.out.println("测试用eclipse提交在项目中新增一个文件同时修改一个文件，操作步骤");
+		}
+	}
+
+}
