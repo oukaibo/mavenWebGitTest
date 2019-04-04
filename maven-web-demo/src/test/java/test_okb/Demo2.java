9b8ab3a598fd4c00e5c98eb6e997125a717a00f1
bokaiou <okb2017@163.com>
2019-04-04 13:10:51 +0800

    '鍦╡clipse鍛戒护绐楀彛锛宲ush涓€娆★紝骞舵瘮杈僡dd-u鍜宎dd-A鐨勫尯鍒?

---
+++ maven-web-demo/src/test/java/test_okb/Demo2.java
@@ -0,0 +1,13 @@
+package test_okb;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<5;i++) {
+			//测试拉取项目，操作步骤
+			System.out.println("测试在项目中新增一个文件，操作步骤");
+		}
+	}
+
+}
