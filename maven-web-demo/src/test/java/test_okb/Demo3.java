03d7697705369b00db07daec91ad1718250ccfbb
bokaiou <okb2017@163.com>
2019-04-04 15:18:04 +0800

    [add] maven-web-demo/src/test/java/test_okb/Demo3.java

--- maven-web-demo/src/test/java/test_okb/Demo3.java
+++ maven-web-demo/src/test/java/test_okb/Demo3.java
@@ -1,13 +1,12 @@
 package test_okb;
 
-public class Demo2 {
+public class Demo3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		for(int i=0;i<5;i++) {
 			//测试拉取项目，操作步骤
-			System.out.println("测试在项目中新增一个文件，操作步骤");
-			System.out.println("测试checkout");
+			System.out.println("测试在项目中新增一个文件同时修改一个文件，操作步骤");
 		}
 	}
 
